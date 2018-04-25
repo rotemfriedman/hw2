c80c5670f58922a2988683eb75c307901b0c5cef
shaked meitav <shakedmeitav@campus.technion.ac.il>
2018-04-25 17:59:54 +0300

    begin

---
+++ driver.c
@@ -0,0 +1,67 @@
+#include "driver.h"
+#include "team.h"
+#include "season.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct driver {
+    int id;
+    char* driver_name;
+    Team driver_team;
+    int points;
+    Season driver_season;
+};
+
+Driver DriverCreate(DriverStatus* status, char* driver_name, int driverId) {
+    Driver new_driver = malloc(sizeof(*new_driver));
+    if(new_driver == NULL) {
+        *status = DRIVER_MEMORY_ERROR;
+        return NULL;
+    }
+    new_driver->Driver_name = malloc(strlen(driver_name) + 1);
+    if(new_driver->Driver_name == NULL) {
+        *status = DRIVER_MEMORY_ERROR;
+        free(new_driver);
+        return NULL;
+    }
+    new_driver->Id = driverId;
+    strcpy(new_driver->Driver_name, driver_name);
+    *status = DRIVER_STATUS_OK;
+    return new_driver;
+
+}
+
+//Get a driver , and return ptr to his team. If the function get NULL, return NULL*//
+Team DriverGetTeam(Driver driver) {
+    if (driver == NULL) {
+        return NULL;
+    } else {
+        return driver->Driver_team;
+    }
+}
+
+const char* DriverGetName(Driver driver) {
+    return driver->Driver_name;
+}
+
+//Get a driver and return his ID. If we get null we return null.
+int DriverGetId(Driver driver){
+    if (driver==NULL)
+        return NULL
+    return driver->Id;
+
+}
+//the function DriverDestroy Get a driver and free the driver*//
+void DriverDestroy(Driver driver){
+    free(driver);
+}
+
+
+int DriverGetId (Driver driver) {
+    if (driver==NULL)
+        return (int)NULL;
+    else
+        return driver->Id;
+}
+
---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "driver.h"
+#include "team.h"
+#include "season.h"
+#include <assert.h>
+
+int main() {
+    DriverStatus status = DRIVER_STATUS_OK;
+    Driver d1 = {123, "avi", NULL, 4, NULL};
+    int check-=DriverGetId(d1);
+    printf("%d", check);
+    //Driver d1 = DriverCreate(&status, "driver", 123);
+    //assert(status == DRIVER_STATUS_OK);
+    //assert(d1 != NULL);
+    // assert(DriverGetName(d1) == "driver");
+    return 0;
+}
